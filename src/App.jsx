5efc113616279ba26588490e8b422c9ca1b3880a
rebeccakt <155926409+rebeccakt@users.noreply.github.com>
2024-01-08 10:55:29 -0500

    website started

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1 id = "Rebecca">Rebecca</h1>
+      <p id = "subtitle">This is my website</p>
+
+    </div>
+    
+    <div>
+      <a href="https://www.instagram.com/_beccatang?igsh=bTF1bWdxbG11bjl0&utm_source=qr" target="_blank">instagram</a>
+    </div>
+
+    
     </>
   )
-}
-
-export default App
+  }
+  export default App
